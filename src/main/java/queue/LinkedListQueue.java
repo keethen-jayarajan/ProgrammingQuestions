763be91e448bfae51142a9d1683434c73c7d4eb3
keethen-jayarajan <keethenrcm@gmail.com>
2020-12-11 11:27:22 -0500

    Update LinkedListQueue.java

--- src/main/java/queue/LinkedListQueue.java
+++ src/main/java/queue/LinkedListQueue.java
@@ -1,3 +1,44 @@
 package queue;
 
 // YOUR CODE GOES HERE
+class LinkedListQueue <T>{
+    class DLLNode {
+		public T name;
+		public DLLNode next;
+        public DLLNode prev;
+        
+    }
+    
+    private DLLNode head = null;
+    private DLLNode tail = null;
+    
+    public void enqueue(T name){
+        DLLNode newNode = new DLLNode();
+        newNode.name = name;
+        newNode.next = tail.next;
+        tail = newNode;
+    }
+
+    public void dequeue(){
+        head = null;
+    }
+
+    public int size(){
+        int size = 0;
+        DLLNode current = tail.next;
+		while(current != null){
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if (head.prev == tail){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
