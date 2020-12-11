7c0de7155e9693e27e52a9a8b13b70df4379f55f
keethen-jayarajan <keethenrcm@gmail.com>
2020-12-11 11:25:38 -0500

    Update MaxDC.java

--- src/main/java/divideAndConquer/MaxDC.java
+++ src/main/java/divideAndConquer/MaxDC.java
@@ -1,5 +1,32 @@
 package divideAndConquer;
+import java.util.*;
 
 public class MaxDC {
-    // YOUR CODE GOES HERE
+    public static int maxDivideAndConquer(int[] arr){
+        if (arr.length == 2){
+            if (arr[0] > arr[1]){
+                return arr[0];
+            }
+            else{
+                return arr[1];
+            }
+        }
+        else if (arr.length == 1){
+            return arr[0];
+        }
+        
+        int[] firstHalf = new int[arr.length/2];
+        for (int i = 0; i < firstHalf.length; i++){
+            firstHalf[i] = arr[i];
+        }
+        int[] secondHalf = new int[arr.length/2 + 1];
+        for (int i = 0; i < secondHalf.length; i++){
+            secondHalf[i] = arr[i + arr.length/2 - 1];
+        }
+
+        System.out.println(maxDivideAndConquer(firstHalf));
+        System.out.println(maxDivideAndConquer(secondHalf));
+        System.out.println();
+        return maxDivideAndConquer(firstHalf);
+    }
 }
